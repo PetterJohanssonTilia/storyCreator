a35c7a106ac299885b0b20a4984484df3c5b4f92
dolmerfresh <146840972+dolmerfresh@users.noreply.github.com>
2024-05-07 11:42:37 +0200

    function: Loop throught .txt and index the words in <>

--- storycreator.py
+++ storycreator.py
@@ -2,4 +2,21 @@
 with open("story.txt", "r") as storytxt:
     story = storytxt.read()
 
-print(story)
+#store the words
+words =[]
+start_of_word = -1
+target_start = "<"
+target_end = ">"
+
+#Finds the place and element of the words
+for i, char in enumerate(story):
+    if char == target_start:
+        start_of_word = i #Stores the index of start_of_word
+
+    if char == target_end and start_of_word != -1: #Only targets words with -1 value
+        word = story[start_of_word: i + 1] # adds the value of +1 to the word
+        words.append(word) #adds the word to the list
+        start_of_word = -1 #resets start_of_word to -1 to find the next word
+
+print(words)
+
